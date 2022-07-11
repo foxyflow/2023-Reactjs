853434a97dcb6cb2b35cf1f0daa9657a0e8a49ce
Foxyflow <foxyflow@contractor.net>
2022-07-11 20:02:07 +1300

    second commit

---
+++ index.js
@@ -0,0 +1,49 @@
+//Get business card done. Try add some 3d to it ... then deploy on foxyflow.net (Vercel) and link in Portfolio. 
+//Add note: Plastering site hosted by Wordpress. Add into lukeslangaugelearning "New Reactjs/threejs 3D site under construction"
+//Reactjs Crash Course Deploy a business card: Make a video explaining React and why I'm focusing on it for my web contracting stack.
+//"Standout from the rest" -- and teach how to make a 3d business card and deploy a website for your portfolio. 
+//Add in typescript.
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+function Page(){ 
+    return ( 
+    <div>
+        <img src="./reactlogo.png" width = "40px"></img>
+        <h1>3D website under construction</h1>
+        
+            <ul>
+            <li>
+                Crash Course for modern 2023 web development coming soon...
+                </li>
+                <li>
+                Contents planned: It will have a customisable 3D business card using React hooks.
+                </li>
+                <li>
+                Great for fullstack work / portfolio.
+                </li>
+                <li>
+                Will show you how to create a composiable webapp with Firebase handling the backend.
+                </li>
+                <li>
+                Video tutorial link coming soon with Vercel deployment. Beginning to end in one video.
+                </li>
+            </ul>
+        
+    
+    </div>
+    );
+}
+
+
+
+
+
+
+ReactDOM.render(
+<Page/>,
+document.getElementById('root'));
+
+
+
+
